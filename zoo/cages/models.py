6df36416736a6fa4f6e5dbe0d206a51e63d38bcb
Hawk94 <x99tom.miller@gmail.com>
2016-07-03 20:09:36 +0100

    sorting out database

--- zoo/cages/models.py
+++ zoo/cages/models.py
@@ -19,20 +19,13 @@ class Choice(models.Model):
 
 
 class ZooCage(models.Model):
-
     #uuid = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-
     created_at = models.DateTimeField(default=timezone.now)
     updated_at = models.DateTimeField(auto_now=True)
     deleted_at = models.DateTimeField(null=True, blank=True)
-
+    cage_type = models.CharField(max_length=200)
+    cage_name = models.CharField(max_length=200)
     cage_contents = []
-
-    def __init__(self, cage_type, cage_name):
-        self.cage_type = cage_type
-        self.cage_name = cage_name
-        self.description = "This shape has not been described yet"
-        self.author = "Nobody has claimed to make this shape yet"
 
     def add_animal(self, animals):
         self.cage_contents.append(animals)
