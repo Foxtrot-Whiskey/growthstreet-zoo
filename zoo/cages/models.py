1e812104309f6709b22d4054a647071ba8ab4d63
Hawk94 <x99tom.miller@gmail.com>
2016-07-03 19:34:42 +0100

    made cages app

--- zoo/cages/models.py
+++ zoo/cages/models.py
@@ -1,10 +1,38 @@
-import uuid
-from django.db import models
+from __future__ import unicode_literals
 
-class ZooCage(models.Model):
-     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-     created_at = models.DateTimeField(default=timezone.now)
-     updated_at = models.DateTimeField(auto_now=True)
-     deleted_at = models.DateTimeField(null=True, blank=True)
-     cage_type = models.CharField(max_length=30)
-     cage_name = models.CharField(max_length=30)
+from django.db import models
+from django.utils import timezone
+
+import datetime
+import uuid
+
+
+class ZooCage:
+
+    #uuid = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+
+    created_at = models.DateTimeField(default=timezone.now)
+    updated_at = models.DateTimeField(auto_now=True)
+    deleted_at = models.DateTimeField(null=True, blank=True)
+
+    cage_contents = []
+
+    def __init__(self, cage_type, cage_name):
+        self.cage_type = cage_type
+        self.cage_name = cage_name
+        self.description = "This shape has not been described yet"
+        self.author = "Nobody has claimed to make this shape yet"
+
+    def add_animal(self, animals):
+        self.cage_contents.append(animals)
+
+
+
+class ZooAnimal():
+
+    #uuid = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    created_at = models.DateTimeField(default=timezone.now)
+    updated_at = models.DateTimeField(auto_now=True)
+    deleted_at = models.DateTimeField(null=True, blank=True)
+    animal_type = models.CharField(max_length=30)
+    animal_name = models.CharField(max_length=30)
